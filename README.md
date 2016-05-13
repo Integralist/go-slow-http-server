69f77e77cb5835f3fbe4b42e4e3b0355d15b7f0c
Integralist <mark.mcdx@gmail.com>
2016-05-13 06:52:13 -0400

    Updates

--- README.md
+++ README.md
@@ -8,8 +8,10 @@ I needed a simple HTTP server that would take a long time to serve responses
 go run slow-server.go
 ```
 
-## Example URL
+## Example
 
-```bash
-http://localhost:3000/pugs
+```
+http://localhost:3000/       # Usage example /foo/5
+http://localhost:3000/pugs   # I love pugs! (no delay)
+http://localhost:3000/pugs/5 # I love pugs! (delayed by 5 seconds)
 ```
--- slow-server.go
+++ slow-server.go
@@ -3,12 +3,32 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	time.Sleep(5000 * time.Millisecond)
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	sections := strings.Split(r.URL.Path, "/")
+	if len(sections) > 2 {
+		delay, err := strconv.Atoi(sections[2])
+		if err != nil {
+			fmt.Fprintf(w, "Hmm, looks like there was a problem with your URL")
+		}
+
+		time.Sleep(time.Duration(delay) * time.Second)
+		fmt.Fprintf(w, "I love %s! (delayed by %d seconds)", sections[1], delay)
+		return
+	}
+
+	if len(sections) > 1 {
+		item := r.URL.Path[1:]
+		if item != "" {
+			fmt.Fprintf(w, "I love %s! (no delay)", item)
+		} else {
+			fmt.Fprintf(w, "Usage example /foo/5")
+		}
+	}
 }
 
 func main() {
